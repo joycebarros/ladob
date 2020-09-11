f3b47594304edbf18c98235b44b5feacc127cb8a
Joyce <joyce_barros@yahoo.com.br>
2020-09-11 15:52:13 -0300

    Criando o projeto

---
+++ src/main/java/com/br/recode/restaurante/ladob/LadobApplication.java
@@ -0,0 +1,13 @@
+package com.br.recode.restaurante.ladob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/recode/restaurante/ladob/LadobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.recode.restaurante.ladob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
