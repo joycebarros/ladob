fdede51f1e6e0447ad2b8eb19736ba02620c3567
elianbalbino <elianbalbino@gmail.com>
2020-09-14 20:49:20 -0300

    atualizando pedido_produto

--- src/main/java/com/br/recode/restaurante/ladob/entidades/pedido_produto.java
+++ src/main/java/com/br/recode/restaurante/ladob/entidades/pedido_produto.java
@@ -1,5 +1,46 @@
 package com.br.recode.restaurante.ladob.entidades;
 
-public class pedido_produto {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table (name="pedido_produto")
+
+
+public class pedido_produto {
+		
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public pedido_produto() {
+		super();
+	}
+
+	public pedido_produto(Long id, Integer quantidade) {
+		super();
+		this.id = id;
+		this.quantidade = quantidade;
+	}
+
+	@Id 
+	@Column 
+	private Long id;
+	
+	@Column
+	private Integer quantidade ;
 }
