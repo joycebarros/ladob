c3c8f3bf923eb619a1d84c39775340d56acb50db
DESKTOP-R0FJ6N1\Marcelo <marcelomogueco@gmail.com>
2020-09-15 20:21:26 -0300

    Commit de criacao  di repository, tables, products, costumers

--- src/main/java/com/br/recode/restaurante/ladob/repository/ProductsRepository.java
+++ src/main/java/com/br/recode/restaurante/ladob/repository/ProductsRepository.java
@@ -3,10 +3,10 @@ package com.br.recode.restaurante.ladob.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.br.recode.restaurante.ladob.model.Costumers;
+import com.br.recode.restaurante.ladob.model.Products;
 
 @Repository
-public interface CostumersRepository  extends JpaRepository<Costumers, Long>{
+public interface ProductsRepository extends JpaRepository<Products, Long>{
 	
 	
 
--- src/main/java/com/br/recode/restaurante/ladob/repository/TablesRepository.java
+++ src/main/java/com/br/recode/restaurante/ladob/repository/TablesRepository.java
@@ -3,11 +3,10 @@ package com.br.recode.restaurante.ladob.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.br.recode.restaurante.ladob.model.Products;
+import com.br.recode.restaurante.ladob.model.Tables;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Products, Long>{
-	
+public interface TablesRepository  extends JpaRepository<Tables, Integer>{
 	
 
 }
