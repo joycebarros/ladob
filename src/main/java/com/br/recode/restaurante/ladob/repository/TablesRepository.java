28d67bce05e11ff19ef900c5959080fe77212237
elianbalbino <elianbalbino@gmail.com>
2020-09-15 21:15:33 -0300

    Criando metodos tables, orderRepositorys e costumersRepositorys

--- src/main/java/com/br/recode/restaurante/ladob/repository/TablesRepository.java
+++ src/main/java/com/br/recode/restaurante/ladob/repository/TablesRepository.java
@@ -1,12 +1,14 @@
 package com.br.recode.restaurante.ladob.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.br.recode.restaurante.ladob.model.Orders;
 import com.br.recode.restaurante.ladob.model.Tables;
 
 @Repository
 public interface TablesRepository  extends JpaRepository<Tables, Integer>{
-	
-
+		List<Orders> findByCodeEquals(String code);
 }
