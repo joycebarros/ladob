a85e5a6b87a69dda900958d34f1d78dad94393c4
DESKTOP-R0FJ6N1\Marcelo <marcelomogueco@gmail.com>
2020-09-15 21:16:01 -0300

    commit de criação de consulta dos produtos

--- src/main/java/com/br/recode/restaurante/ladob/repository/ProductsRepository.java
+++ src/main/java/com/br/recode/restaurante/ladob/repository/ProductsRepository.java
@@ -1,5 +1,7 @@
 package com.br.recode.restaurante.ladob.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,8 @@ import com.br.recode.restaurante.ladob.model.Products;
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long>{
 	
+	List<Products> findByNameContainingIgnoreCase(String product);
 	
+	//List<Products> findBy(Long id);
 
 }
