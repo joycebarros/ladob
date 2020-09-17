bff3c47a7237d0e054284905c4968ece4bf912c4
Marcela Queiroz <marcelapfqueiroz@gmail.com>
2020-09-17 19:35:06 -0300

    Criando CostumersController

--- src/main/java/com/br/recode/restaurante/ladob/controller/CostumersController.java
+++ src/main/java/com/br/recode/restaurante/ladob/controller/CostumersController.java
@@ -1,5 +1,20 @@
 package com.br.recode.restaurante.ladob.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.recode.restaurante.ladob.service.CostumersService;
+
+@RestController
+@RequestMapping (path = "/costumers", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CostumersController {
 
+	private CostumersService costumersService;
+	
+	public CostumersController(CostumersService costumersService) {
+		super();
+		this.costumersService = costumersService;
+	}
+	
 }
