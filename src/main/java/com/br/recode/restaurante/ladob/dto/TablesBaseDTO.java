b0116a387d092f90108981efbe043926176f00cb
Joyce <joyce_barros@yahoo.com.br>
2020-10-27 20:57:30 -0300

    Corrigindo o merge da classe TablesBaseDTO.

--- src/main/java/com/br/recode/restaurante/ladob/dto/TablesBaseDTO.java
+++ src/main/java/com/br/recode/restaurante/ladob/dto/TablesBaseDTO.java
@@ -1,12 +1,10 @@
 package com.br.recode.restaurante.ladob.dto;
 
 public class TablesBaseDTO {
-<<<<<<< HEAD
 	
 	private Integer id;
 	private String code;
 	private Integer seats;
-	
 	
 	public TablesBaseDTO() {
 		super();
@@ -37,44 +35,4 @@ public class TablesBaseDTO {
 		this.seats = seats;
 	}
 
-
-	
-	
-
-=======
-
-	private Integer id;
-	private String code;
-	private Integer seats;
-	
-	public TablesBaseDTO() {
-		
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public Integer getSeats() {
-		return seats;
-	}
-
-	public void setSeats(Integer seats) {
-		this.seats = seats;
-	}
-	
-	
->>>>>>> refs/heads/ladob_joycebarros
 }
